df250343736b0616130e32d71ea554c480fd4c03
Salihxmod <semetsalih344@gmail.com>
2026-02-19 19:50:01 +0300

    feat(api): configure global CORS policy for frontend client integration
    -Added 'AllowAll' CORS policy in Program.cs to permit cross-origin requests.
    -Configured policy to allow any origin, any HTTP method, and any header.
    -Registered UseCors middleware into the HTTP request pipeline.

--- Notla/Notla.API/Program.cs
+++ Notla/Notla.API/Program.cs
@@ -60,6 +60,15 @@ builder.Services.Configure<ApiBehaviorOptions>(options =>
 });
 builder.Services.AddFluentValidationAutoValidation()
     .AddValidatorsFromAssemblyContaining<NoteCreateDtoValidator>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -70,7 +79,7 @@ if (app.Environment.IsDevelopment())
 }
 app.UseCustomException();
 app.UseHttpsRedirection();
-
+app.UseCors("AllowAll");
 app.MapControllers();
 
 app.Run();
